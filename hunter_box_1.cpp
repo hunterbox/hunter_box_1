0395e45c994accbbec1b584df574841d15409ee3
hunterbox <hunterbox@users.noreply.github.com>
2017-09-16 21:10:23 +0300

    Add code

---
+++ hunter_box_1.cpp
@@ -0,0 +1,9 @@
+#include <hunter_box_1.hpp>
+
+namespace hunter_box_1 {
+
+int foo() {
+  return 0x42;
+}
+
+} // namespace hunter_box_1
---
+++ hunter_box_1.hpp
@@ -0,0 +1,10 @@
+#ifndef HUNTER_BOX_1_HPP_
+#define HUNTER_BOX_1_HPP_
+
+namespace hunter_box_1 {
+
+int foo();
+
+} // namespace hunter_box_1
+
+#endif // HUNTER_BOX_1_HPP_
